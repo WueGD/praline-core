e8d3769a3c8131f4c4b91356bd85881e4f144ee9
Johannes Zink <johannes.zink@uni-wuerzburg.de>
2019-11-19 14:07:31 +0100

    Added javadoc for method setVertex in interface PortComposition

--- Praline-Data-Structure/src/main/java/de/uniwue/informatik/praline/datastructure/graphs/PortComposition.java
+++ Praline-Data-Structure/src/main/java/de/uniwue/informatik/praline/datastructure/graphs/PortComposition.java
@@ -3,5 +3,13 @@ package de.uniwue.informatik.praline.datastructure.graphs;
 public interface PortComposition {
 
     Vertex getVertex();
+
+    /**
+     * Should not be manually called.
+     * This method is primarily made for the class {@link Vertex}
+     * which calls it when a {@link PortComposition} is added or removed to it
+     *
+     * @param vertex
+     */
     void setVertex(Vertex vertex);
 }
