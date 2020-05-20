507b1b33b37cd9d812d75e8f201defe875add3af
Johannes Zink <johannes.zink@uni-wuerzburg.de>
2020-05-20 13:28:09 +0200

    filter out duplicate entries when creating new lists

--- Praline-Data-Structure/src/main/java/de/uniwue/informatik/praline/datastructure/utils/GraphUtils.java
+++ Praline-Data-Structure/src/main/java/de/uniwue/informatik/praline/datastructure/utils/GraphUtils.java
@@ -5,13 +5,30 @@ import java.util.Collection;
 
 public class GraphUtils {
 
+    /**
+     * Creates a list of the passed elements.
+     * It filters out entries that are null and duplicate entries.
+     * If the element parameter is null then it returns an empty list.
+     *
+     * @param elements
+     * @param <T>
+     * @return
+     */
     public static <T> ArrayList<T> newArrayListNullSafe(Collection<T> elements) {
         if (elements == null) {
             return new ArrayList<>();
         }
-        while (elements.contains(null)) {
-            elements.remove(null);
+
+        ArrayList<T> returnList = new ArrayList<>(elements);
+        for (T element : elements) {
+            if (element == null) {
+                continue;
+            }
+            if (!returnList.contains(element)) {
+                returnList.add(element);
+            }
         }
-        return new ArrayList<>(elements);
+
+        return returnList;
     }
 }
