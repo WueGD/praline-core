3114702d643b28ddce286b9f5af6e18ebbbd9dcc
Johannes Zink <johannes.zink@uni-wuerzburg.de>
2023-03-23 15:05:07 +0100

    fix: arrange multiple port pairings at one node in a bipartite way on NORTH and SOUTH side

--- Praline-Layouting/src/main/java/de/uniwue/informatik/praline/layouting/layered/algorithm/layerassignment/PortSideAssignment.java
+++ Praline-Layouting/src/main/java/de/uniwue/informatik/praline/layouting/layered/algorithm/layerassignment/PortSideAssignment.java
@@ -6,6 +6,7 @@ import de.uniwue.informatik.praline.datastructure.utils.PortUtils;
 import de.uniwue.informatik.praline.layouting.layered.algorithm.SugiyamaLayouter;
 import de.uniwue.informatik.praline.layouting.layered.algorithm.util.Constants;
 import de.uniwue.informatik.praline.layouting.layered.algorithm.util.SortingOrder;
+import edu.uci.ics.jung.graph.util.Pair;
 
 import java.util.*;
 
@@ -38,23 +39,45 @@ public class PortSideAssignment {
             List<PortComposition> portCompositionsTop = new ArrayList<>();
             List<PortComposition> portCompositionsBottom = new ArrayList<>();
             Set<PortComposition> freePortCompositions = new LinkedHashSet<>();
-            for (PortComposition portComposition : node.getPortCompositions()) {
-                //for each port composition (usually a port group), compute a score that equals the number of edges
+
+            List<Collection<PortComposition>> allPCsGrouped = new ArrayList<>(node.getPortCompositions().size());
+            if (sugy.isPlug(node)) {
+                // special case: if we have a plug, we need some port( group)s to be on the same side
+                Pair<Set<PortComposition>> pairedPCs = fixPortSidesOfPlug(node);
+                allPCsGrouped.add(pairedPCs.getFirst());
+                allPCsGrouped.add(pairedPCs.getSecond());
+                //find the other pcs of this plug that are not paired and also add them
+                for (PortComposition pc : node.getPortCompositions()) {
+                    if (!pairedPCs.getFirst().contains(pc) && !pairedPCs.getSecond().contains(pc)) {
+                        allPCsGrouped.add(Collections.singleton(pc));
+                    }
+                }
+            }
+            else {
+                for (PortComposition pc : node.getPortCompositions()) {
+                    allPCsGrouped.add(Collections.singleton(pc));
+                }
+            }
+
+
+            for (Collection<PortComposition> portCompositionGroup : allPCsGrouped) {
+                //for each collection of port compositions (usually a single port group), compute a score that equals
+                // the number of edges
                 // going upwards minus the number of edges going downwards. Depending on the sign of the score, we
-                // will assign the port composition
-                //Maybe vertex side is also predefined, then set it to a positive or negative value first
-                int score = predefinedPortSide(portComposition);
+                // will assign the port composition.
+                // Maybe vertex side is also predefined, then set it to a positive or negative value first
+                int score = predefinedPortSide(portCompositionGroup);
                 if (score == 0) {
-                    score = countEdgesUpwardsMinusEdgesDownwards(portComposition);
+                    score = countEdgesUpwardsMinusEdgesDownwards(portCompositionGroup);
                 }
                 //assign to side acc. to score
                 if (score < 0) {
-                    portCompositionsBottom.add(portComposition);
+                    portCompositionsBottom.addAll(portCompositionGroup);
                 }
                 else if (score > 0) {
-                    portCompositionsTop.add(portComposition);
+                    portCompositionsTop.addAll(portCompositionGroup);
                 } else {
-                    freePortCompositions.add(portComposition);
+                    freePortCompositions.addAll(portCompositionGroup);
                 }
             }
             // handle PortCompositions with no edges by adding them to the side with fewer ports
@@ -66,10 +89,6 @@ public class PortSideAssignment {
                 } else {
                     portCompositionsBottom.add(portComposition);
                 }
-            }
-            // special case: if we have a plug, there cannot be both port groups on the same side
-            if (sugy.isPlug(node)) {
-                repairPortSidesOfPlug(portCompositionsTop, portCompositionsBottom);
             }
 
             List<Port> topPorts = PortUtils.getPortsRecursively(portCompositionsTop);
@@ -89,18 +108,20 @@ public class PortSideAssignment {
 
     /**
      *
-     * @param portComposition
+     * @param portCompositions
      * @return
      *      a negative value if portComposition has more South side ports than North side ports,
      *      a positive value if portComposition has fewer South side ports than North side ports,
      *      and 0 if it has equally many or no North or South side ports.
      */
-    private int predefinedPortSide(PortComposition portComposition) {
+    private int predefinedPortSide(Collection<PortComposition> portCompositions) {
         boolean hasNorthSidePorts = false;
         boolean hasSouthSidePorts = false;
+        Vertex northSidePortVertex = null;
+        Vertex southSidePortVertex = null;
 
         int score = 0;
-        for (Port port : PortUtils.getPortsRecursively(portComposition)) {
+        for (Port port : PortUtils.getPortsRecursively(portCompositions)) {
             if (port.getOrientationAtVertex() == Orientation.WEST
                     || port.getOrientationAtVertex() == Orientation.EAST) {
                 System.out.println("Warning! Port " + port + " at vertex " + port.getVertex() + " has orientation " +
@@ -108,66 +129,121 @@ public class PortSideAssignment {
             }
             else if (port.getOrientationAtVertex() == Orientation.NORTH) {
                 hasNorthSidePorts = true;
+                northSidePortVertex = port.getVertex();
                 ++score;
             }
             else if (port.getOrientationAtVertex() == Orientation.SOUTH) {
                 hasSouthSidePorts = true;
+                southSidePortVertex = port.getVertex();
                 --score;
             }
         }
         if (hasNorthSidePorts && hasSouthSidePorts) {
-            System.out.println("Warning! A port group at vertex " + portComposition.getVertex() + " has both, ports " +
+            String vertexName = northSidePortVertex == southSidePortVertex ? northSidePortVertex.toString() :
+                    northSidePortVertex + " and vertex " + southSidePortVertex;
+            System.out.println("Warning! A port group at vertex " + vertexName + " has both, ports " +
                     "assigned to " + Orientation.NORTH + " and to " + Orientation.SOUTH + ".");
         }
 
         return score;
     }
 
-    private void repairPortSidesOfPlug(List<PortComposition> portCompositionsTop,
-                                       List<PortComposition> portCompositionsBottom) {
-        if (portCompositionsBottom.isEmpty()) {
-            int lowestScore = Integer.MAX_VALUE;
-            PortComposition pcLowestScore = null;
-            for (PortComposition portComposition : portCompositionsTop) {
-                int score = countEdgesUpwardsMinusEdgesDownwards(portComposition);
-                if (score < lowestScore) {
-                    lowestScore = score;
-                    pcLowestScore = portComposition;
-                }
-            }
-            portCompositionsTop.remove(pcLowestScore);
-            portCompositionsBottom.add(pcLowestScore);
-        }
-        if (portCompositionsTop.isEmpty()) {
-            int highestScore = Integer.MIN_VALUE;
-            PortComposition pcHighestScore = null;
-            for (PortComposition portComposition : portCompositionsBottom) {
-                int score = countEdgesUpwardsMinusEdgesDownwards(portComposition);
-                if (score > highestScore) {
-                    highestScore = score;
-                    pcHighestScore = portComposition;
-                }
-            }
-            portCompositionsBottom.remove(pcHighestScore);
-            portCompositionsTop.add(pcHighestScore);
-        }
+    private int countEdgesUpwardsMinusEdgesDownwards(PortComposition portComposition) {
+        return countEdgesUpwardsMinusEdgesDownwards(Collections.singleton(portComposition));
     }
 
-    private int countEdgesUpwardsMinusEdgesDownwards(PortComposition portComposition) {
-        Vertex node = portComposition.getVertex();
+    private int countEdgesUpwardsMinusEdgesDownwards(Collection<PortComposition> portCompositions) {
         int score = 0;
-        if (portComposition instanceof Port) {
-            for (Edge edge : ((Port) portComposition).getEdges()) {
-                if (!sugy.staysOnSameLayer(edge)) {
-                    score += sugy.getStartNode(edge).equals(node) ? 1 : -1;
+        for (PortComposition pc : portCompositions) {
+            Vertex node = pc.getVertex();
+            if (pc instanceof Port) {
+                for (Edge edge : ((Port) pc).getEdges()) {
+                    if (!sugy.staysOnSameLayer(edge)) {
+                        score += sugy.getStartNode(edge).equals(node) ? 1 : -1;
+                    }
                 }
-            }
-        }
-        else if (portComposition instanceof PortGroup) {
-            for (PortComposition subPortComposition : ((PortGroup) portComposition).getPortCompositions()) {
-                score += countEdgesUpwardsMinusEdgesDownwards(subPortComposition);
+            } else if (pc instanceof PortGroup) {
+                for (PortComposition subPortComposition : ((PortGroup) pc).getPortCompositions()) {
+                    score += countEdgesUpwardsMinusEdgesDownwards(subPortComposition);
+                }
             }
         }
         return score;
+    }
+
+
+    private Pair<Set<PortComposition>> fixPortSidesOfPlug(Vertex plug) {
+        List<Pair<PortComposition>> pairedPairs = getPairedPairs(plug.getPortCompositions());
+
+        Set<PortComposition> firstSide = new LinkedHashSet<>();
+        Set<PortComposition> secondSide = new LinkedHashSet<>();
+
+        while (!pairedPairs.isEmpty()) {
+            //try to find a pair where we already know 1 endpoint
+            Pair<PortComposition> nextPair = pairedPairs.get(0);
+            for (Pair<PortComposition> pairedPair : pairedPairs) {
+                if (firstSide.contains(pairedPair.getFirst()) || firstSide.contains(pairedPair.getSecond()) ||
+                        secondSide.contains(pairedPair.getFirst()) || secondSide.contains(pairedPair.getSecond())) {
+                    nextPair = pairedPair;
+                    break;
+                }
+            }
+            //now resolve this pair and remove it from the remaining paired pairs
+            PortComposition first = nextPair.getFirst();
+            PortComposition second = nextPair.getSecond();
+            pairedPairs.remove(nextPair);
+
+            if ((firstSide.contains(first) && firstSide.contains(second)) ||
+                    (secondSide.contains(first) && secondSide.contains(second))) {
+                System.out.println("Warning! Port Pairings at " + plug + " are not paired in a bipartite way. Some " +
+                        "port pairings could not be added.");
+                continue;
+            }
+
+            if (firstSide.contains(first)) {
+                secondSide.add(second);
+            }
+            else if (firstSide.contains(second)) {
+                secondSide.add(first);
+            }
+            else if (secondSide.contains(first)) {
+                firstSide.add(second);
+            }
+            else if (secondSide.contains(second)) {
+                firstSide.add(first);
+            }
+            else {
+                firstSide.add(first);
+                secondSide.add(second);
+            }
+        }
+
+        return new Pair<>(firstSide, secondSide);
+    }
+
+    private List<Pair<PortComposition>> getPairedPairs(List<PortComposition> portCompositions) {
+        List<Pair<PortComposition>> allConflictingPairs = new LinkedList<>();
+        for (int i = 0; i < portCompositions.size() - 1; i++) {
+            for (int j = i + 1; j < portCompositions.size(); j++) {
+                PortComposition pc0 = portCompositions.get(i);
+                PortComposition pc1 = portCompositions.get(j);
+                if (havePortPairing(pc0, pc1)) {
+                    allConflictingPairs.add(new Pair<>(pc0, pc1));
+                }
+            }
+        }
+        return allConflictingPairs;
+    }
+
+     private boolean havePortPairing(PortComposition pc0, PortComposition pc1) {
+        List<Port> ports0 = PortUtils.getPortsRecursively(pc0);
+        List<Port> ports1 = PortUtils.getPortsRecursively(pc1);
+        for (Port port0 : ports0) {
+            Port pairedPort = sugy.getPairedPort(port0);
+            if (pairedPort != null && ports1.contains(pairedPort)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
